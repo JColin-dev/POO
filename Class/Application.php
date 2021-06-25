003677b4fa4358697c894b39e8f27603c8649c42
JColin-dev <colin_dev@outlook.fr>
2021-06-25 15:59:46 +0200

    url

--- Class/Application.php
+++ Class/Application.php
@@ -2,16 +2,32 @@
 
 namespace App;
 
-use model\Utilisateur;
+use App\Model\Utilisateur;
 
 class Application {
     public static function demarrer() {
 
-        $john = new Utilisateur("john");
-        // $john = new model\Utilisateur("john"); cela marche aussi
+        //http://localhost/Tests_POO_PHP/Class/article/liste
 
-        $john->nomEntier();
+        $partieUrl = explode("/", $_GET["page"]);
 
+        $nomController = "\\App\\Controller\\".ucfirst($partieUrl[0])."Controller";
+
+        $controleur = new $nomController();
+
+        $controleur->liste();
+
+        /*$nomClass = "App\Model\Utilisateur";
+
+        $utilisateur = new $nomClass("");
+        $utilisateur->nomEntier();
+
+        /*$maFonction = "test";
+        self::$maFonction();*/
 
     }
+
+    /*static function test() {
+        echo 'ça marche';
+    }*/
 }
