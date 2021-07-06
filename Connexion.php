9c3b26a59dfcba55c5774de88e182e34e2734854
JColin-dev <colin_dev@outlook.fr>
2021-07-06 17:00:03 +0200

    recap

--- Connexion.php
+++ Connexion.php
@@ -2,7 +2,7 @@
 
 class Connexion extends PDO{
     public function __construct() {
-        parent::__construct("mysql:dbname=Recap;host=localhost", "root", "");
+        parent::__construct("mysql:dbname=Recap;host=localhost::3306", "root", "");
     }
 }
 
